335a5217260a7e3e258107eba10672df7848bedb
Alejandro Arbiza <alejandro@roetal.com>
2017-07-26 08:00:18 -0400

    Minor change.

--- src/app/config/logger.php
+++ src/app/config/logger.php
@@ -1,7 +1,7 @@
 <?php 
 return function($c) {
     $logFileName = PRIVATE_APP_PATH . DIRECTORY_SEPARATOR . basename(PRIVATE_APP_PATH) . '.log'; 
-    $logger = new \Monolog\Logger('pwless');
+    $logger = new \Monolog\Logger('chubby');
     $file_handler = new \Monolog\Handler\StreamHandler( $logFileName );
     $logger->pushHandler($file_handler);
     return $logger;   
