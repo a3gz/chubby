66f4e394801dc3d4a368bf35839819ea6e530495
Alejandro Arbiza <alejandro@roetal.com>
2020-07-25 09:55:14 -0400

    Remove `Dockerfile` and `docker-compose.yml` from version control
     * Remove the mentioned files.
     * Create `.sample` versions to include in version control.
     * Update `.gitignore` to take these changes into account.
    
    	modified:   .gitignore
    	renamed:    Dockerfile -> Dockerfile.sample
    	deleted:    docker-compose.yml
    	deleted:    src/.gitignore
    	modified:   src/app/views/templates/DefaultTemplate.php

--- src/app/views/templates/DefaultTemplate.php
+++ src/app/views/templates/DefaultTemplate.php
@@ -1,11 +1,11 @@
 <?php
+
 namespace Templates;
 
-class DefaultTemplate extends \Chubby\View\Template
-{
+class DefaultTemplate extends \Chubby\View\Template {
   protected $components = [
-      'header'    => 'views/components/header.php',
-      'footer'    => 'views/components/footer.php',
+    'header'    => 'views/components/header.php',
+    'footer'    => 'views/components/footer.php',
   ];
 
   protected $template = 'views/templates/default-template.php';
