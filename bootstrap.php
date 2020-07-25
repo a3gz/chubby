bd280765a6098725ce67874342608b2c015e2d00
Alejandro Arbiza <alejandro@roetal.com>
2020-07-25 10:00:35 -0400

    Re-organize code in entry point files
    	modified:   bootstrap.php
    	modified:   index.php

--- bootstrap.php
+++ bootstrap.php
@@ -1,18 +1,58 @@
 <?php
-defined('PUBLIC_APP_PATH') || define('PUBLIC_APP_PATH', __DIR__);
-defined('PRIVATE_APP_PATH') || define('PRIVATE_APP_PATH', PUBLIC_APP_PATH);
+defined('PUBLIC_APP_PATH') || define('PUBLIC_APP_PATH',   __DIR__);
+defined('PRIVATE_APP_PATH') || define('PRIVATE_APP_PATH',  PUBLIC_APP_PATH);
+
+defined('THEMES_ROOT') || define(
+  'THEMES_ROOT',
+  PUBLIC_APP_PATH . '/assets/themes'
+);
 
 // Directories usually kept in the private directory.
 defined('APP_PATH') || define('APP_PATH', PRIVATE_APP_PATH . '/src/app');
-defined('VENDOR_PATH') || define('VENDOR_PATH', PRIVATE_APP_PATH . '/src/vendor');
-defined('CONFIG_PATH') || define('CONFIG_PATH', PRIVATE_APP_PATH . '/src/app/config');
+defined('VENDOR_PATH') || define(
+  'VENDOR_PATH',
+  PRIVATE_APP_PATH . '/src/vendor'
+);
+defined('ROUTES_PATH') || define(
+  'ROUTES_PATH',
+  PRIVATE_APP_PATH . '/src/app/routes'
+);
+defined('CONFIG_PATH') || define(
+  'CONFIG_PATH',
+  PRIVATE_APP_PATH . '/src/app/config'
+);
 defined('LOG_PATH') || define('LOG_PATH', PRIVATE_APP_PATH . '/src/log');
 
-// Special route files locations
-defined('ROUTES_PATH') || define('ROUTES_PATH', PRIVATE_APP_PATH . '/src/app/routes');
-defined('CONSOLE_ROUTES_PATH') || define(
-  'CONSOLE_ROUTES_PATH',
-  PRIVATE_APP_PATH . '/src/app/routes/console'
-);
+define('CONSOLE_ROUTES_PATH', APP_PATH . '/routes/console');
 
 include VENDOR_PATH . '/autoload.php';
+
+$__ = [];
+
+/**
+ * Grab config.php and use those settings to create the Slim application.
+ */
+$__['cfgPath'] = CONFIG_PATH;
+$__['config'] = include($__['cfgPath'] . '/config.php');
+
+$APP = new \Slim\App($__['config']);
+
+$__['container'] = $APP->getContainer();
+$__['dir'] = scandir($__['cfgPath']);
+/**
+ * If loading order is important, we can prefix all configuration files
+ * (except for config.php) with a number and a dash, like so: 001-file1.php,
+ * 002-file2.php, etc.
+ * The prefix will be removed so only the part of the name after the first
+ * dash will be considered as the file's real name.
+ */
+sort($__['dir']);
+foreach ($__['dir'] as $__fileName) {
+  if ((substr($__fileName, 0, 1) == '.') || ($__fileName == 'config.php')) continue;
+  $__fullFileName = "{$__['cfgPath']}/{$__fileName}";
+  if (is_readable($__fullFileName)) {
+    $__fileName = preg_replace('#^[0-9]+-#', '', $__fileName);
+    $__key = substr($__fileName, 0, -4); // Remove the file extension
+    $__['container'][$__key] = include $__fullFileName;
+  }
+}
--- index.php
+++ index.php
@@ -1,6 +1,24 @@
 <?php
 include __DIR__ . '/bootstrap.php';
 
+if (defined('CONSOLE') && defined('CONSOLE_ROUTES_PATH')) {
+  $__['container']['environment'] = function($c) {
+    $argv = $GLOBALS['argv'];
+    array_shift($argv);
+    $pathInfo = implode('/', $argv);
+    $env = ['REQUEST_URI' => "/{$pathInfo}"];
+    return \Slim\Http\Environment::mock($env);
+  };
+  $__['path'] = CONSOLE_ROUTES_PATH;
+} else {
+  $__['path'] = ROUTES_PATH;
+}
+
+__chubbyIncludeRoutes($__['path']);
+unset($__); // Destroy all the temporary global variables.
+$APP->run(); // Run Slim, run!
+
+/***************************************************************/
 function __chubbyIncludeRoutes($basePath) {
   global $APP;
   $__['dir'] = scandir($basePath);
@@ -16,50 +34,5 @@ function __chubbyIncludeRoutes($basePath) {
     }
   }
 }
-
-$__ = [];
-$__['cfgPath'] = CONFIG_PATH;
-$__['config'] = include($__['cfgPath'] . '/config.php');
-$__['dir'] = scandir($__['cfgPath']);
-
-$APP = new \Slim\App(['settings' => $__['config']]);
-
-/**
- * If loading order is important, we can prefix all configuration files
- * (except for config.php) with a number and a dash, like so: 001-file1.php,
- * 002-file2.php, etc.
- * The prefix will be removed so only the part of the name after the first dash
- * will be considered as the file's real name.
- */
-sort($__['dir']);
-$__['container'] = $APP->getContainer();
-foreach ($__['dir'] as $__fileName) {
-  if ((substr($__fileName, 0, 1 ) == '.') || ( $__fileName == 'config.php')) {
-    continue;
-  }
-  $__fullFileName = "{$__['cfgPath']}/{$__fileName}";
-  if (is_readable($__fullFileName)) {
-    $__fileName = preg_replace('#^[0-9]+-#', '', $__fileName);
-    $__key = substr($__fileName, 0, -4);
-    $__['container'][$__key] = include $__fullFileName;
-  }
-}
-
-if (defined('CONSOLE') && defined('CONSOLE_ROUTES_PATH')) {
-  $__['container']['environment'] = function($c) {
-    $argv = $GLOBALS['argv'];
-    array_shift($argv);
-    $pathInfo = implode('/', $argv);
-    $env = ['REQUEST_URI' => "/{$pathInfo}"];
-    return \Slim\Http\Environment::mock($env);
-  };
-  $__['path'] = CONSOLE_ROUTES_PATH;
-} else {
-  $__['path'] = ROUTES_PATH;
-}
-
-__chubbyIncludeRoutes($__['path']);
-unset($__);
-$APP->run();
 
 // EOF
