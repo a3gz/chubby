d2726293cd3bbddf5c4ecd2681d641b59444eadf
Alejandro Arbiza <alejandro@roetal.com>
2021-03-31 14:00:38 -0400

    Update `404.php` page
    Better describe how to create a custom 404 page.
    
    	modified:   src/app/views/components/404.php

--- src/app/views/components/404.php
+++ src/app/views/components/404.php
@@ -13,16 +13,17 @@
   <h3>Customize your 404 response:</h3>
   <h4>Option 1</h4>
   <p>Edit the component: <code>src/app/views/components/404.php</code></p>
+  <p>This will use the provided handler which can be found at <code>src/fat/Handlers/NotFoundExceptionHandler.php</code></p>
 
   <h4>Option 2</h4>
   <ul>
     <li>
-      Create a copy of <code>vendor/a3gz/chubby/src/fat/Handlers/NotFoundExceptionHandler.php</code>
-      and paste it somewhere in your application's tree.
+      Edit or create a copy of <code>src/fat/Handlers/NotFoundExceptionHandler.php</code>.
     </li>
     <li>
       Edit your application's <code>src/app/config/config.php</code> 
-      and assign your custom callback under <code>notFoundHandler</code>.
+      and retur your custom callback under <code>notFoundHandler</code>.
     </li>
+    <li>Edit or create a copy of the component: <code>src/app/views/components/404.php</code></li>
   </ul>
 </div>
